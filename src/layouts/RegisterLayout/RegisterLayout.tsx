378db49fd556a496dda47966fd3b12cb0ecb09d2
hoangday185 <jarvietnguyen318@gmail.com>
2024-05-14 01:08:38 +0700

    setting react router

---
+++ src/layouts/RegisterLayout/RegisterLayout.tsx
@@ -0,0 +1,9 @@
+type RegisterLayoutProps = {
+  children?: React.ReactNode
+}
+
+const RegisterLayout = ({ children }: RegisterLayoutProps): JSX.Element => {
+  return <div>RegisterLayout{children}</div>
+}
+
+export default RegisterLayout
---
+++ src/useRouteElements.tsx
@@ -0,0 +1,33 @@
+import { useRoutes } from 'react-router-dom'
+import ProductList from './pages/ProductList'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import RegisterLayout from './layouts/RegisterLayout'
+
+const useRouteElements = () => {
+  const routeElements = useRoutes([
+    {
+      path: '/',
+      element: <ProductList />
+    },
+    {
+      path: '/login',
+      element: (
+        <RegisterLayout>
+          <Login />
+        </RegisterLayout>
+      )
+    },
+    {
+      path: '/register',
+      element: (
+        <RegisterLayout>
+          <Register />
+        </RegisterLayout>
+      )
+    }
+  ])
+  return routeElements
+}
+
+export default useRouteElements
