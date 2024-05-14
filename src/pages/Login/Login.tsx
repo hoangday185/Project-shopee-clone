198ac4bca5929a0312a2fbb31d8d737655e7ba2d
hoangday185 <jarvietnguyen318@gmail.com>
2024-05-14 17:47:51 +0700

    config for plugin tailwind

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -14,7 +14,7 @@ const Login = () => {
 
   return (
     <div className='bg-orange'>
-      <div className='max-w-7xl mx-auto px-4'>
+      <div className='container'>
         <div className='grid grid-cols-1 lg:grid-cols-5 py-12 lg:py-32 lg:pr-10'>
           <div className='lg:col-span-2 lg:col-start-4'>
             <form
