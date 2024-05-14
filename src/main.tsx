2bc08f2a4eb6cec3bc06670c52663cfd2d5fa7a3
hoangday185 <jarvietnguyen318@gmail.com>
2024-05-15 02:35:55 +0700

    config for axios and react-query

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false
+    }
+  }
+})
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
     </BrowserRouter>
   </React.StrictMode>
 )
---
+++ src/utils/http.ts
@@ -0,0 +1,17 @@
+import axios, { type AxiosInstance } from 'axios'
+
+class Http {
+  instance: AxiosInstance
+  constructor() {
+    this.instance = axios.create({
+      baseURL: 'http://api-ecom.duthanhduoc.com/',
+      timeout: 10000,
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+  }
+}
+
+const http = new Http().instance
+export default http
