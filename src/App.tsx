35832ed04d099dd9e3872028209561adb25842d0
hoangday185 <jarvietnguyen318@gmail.com>
2024-05-15 16:08:46 +0700

    handle error with axios interceptor

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
+import { ToastContainer } from 'react-toastify'
 import './App.css'
 import useRouteElements from './useRouteElements'
+import 'react-toastify/dist/ReactToastify.css'
 
 function App() {
   const routeElements = useRouteElements()
   return (
     <>
       <div>{routeElements}</div>
+      <ToastContainer />
     </>
   )
 }
--- src/utils/http.ts
+++ src/utils/http.ts
@@ -1,4 +1,6 @@
-import axios, { type AxiosInstance } from 'axios'
+import axios, { AxiosError, type AxiosInstance } from 'axios'
+import HttpStatusCode from 'src/constants/httpStatusCode.enum'
+import { toast } from 'react-toastify'
 
 class Http {
   instance: AxiosInstance
@@ -10,6 +12,19 @@ class Http {
         'Content-Type': 'application/json'
       }
     })
+    this.instance.interceptors.response.use(
+      function (response) {
+        return response.data
+      },
+      function (error: AxiosError) {
+        if (error.response?.status !== HttpStatusCode.UnprocessableEntity) {
+          const data: any | undefined = error.response?.data
+          const message = data.message || error.message
+          toast.error(message)
+        }
+        return Promise.reject(error)
+      }
+    )
   }
 }
 
