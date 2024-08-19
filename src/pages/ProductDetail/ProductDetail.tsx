6a04284285831e1a8ebc8c5e182f14fdfa185280
hoangday185 <jarvietnguyen318@gmail.com>
2024-08-19 14:39:03 +0700

    feat: slider for image in page product detail

--- src/pages/ProductDetail/ProductDetail.tsx
+++ src/pages/ProductDetail/ProductDetail.tsx
@@ -1,12 +1,12 @@
 import { useQuery } from '@tanstack/react-query'
 import { useParams } from 'react-router-dom'
 import productApi from 'src/apis/product.api'
-import RatingStarts from '../RatingStarts'
 import RatingStar from 'src/components/RatingStar/RatingStar'
 import { formatNumberSold, formatPrice } from 'src/utils/formatNumber'
 import { rateSale } from 'src/utils/utils'
 import InputNumber from 'src/components/InputNumber'
 import DOMPurify from 'dompurify'
+import { useEffect, useMemo, useState } from 'react'
 
 const ProductDetail = () => {
   const { id } = useParams()
@@ -14,13 +14,45 @@ const ProductDetail = () => {
     queryKey: ['product', id],
     queryFn: () => productApi.getProductDetail(id as string)
   })
+  //tạo current index image để set active cho ảnh và lấy ra 5 ảnh đầu tiên
+  const [currentIndexImage, setCurrentIndexImage] = useState<[number, number]>([
+    0, 5
+  ])
+  const [imageActive, setImageActive] = useState<string>('')
 
   const product = data?.data.data
+  const currentImages = useMemo(
+    () => (product ? product.images.slice(...currentIndexImage) : []),
+    [product, currentIndexImage]
+  )
+
+  useEffect(() => {
+    if (product && product.images.length > 0) {
+      //khi có data thì check thêm list ảnh có rỗng không thỏa 2 điều trên thì set vào useState
+      setImageActive(product.images[0])
+    }
+  }, [product])
+
+  const onHoverImage = (image: string) => {
+    setImageActive(image)
+  }
+
+  const nextImage = () => {
+    if (product && product.images.length > currentIndexImage[1]) {
+      setCurrentIndexImage((prev) => [prev[0] + 1, prev[1] + 1])
+    }
+  }
+
+  const prevImage = () => {
+    if (currentIndexImage[0] > 0) {
+      setCurrentIndexImage((prev) => [prev[0] - 1, prev[1] - 1])
+    }
+  }
   if (!product) return null
   return (
     <div className='bg-gray-200 py-6'>
-      <div className='bg-white p-4 shadow'>
-        <div className='container'>
+      <div className='container'>
+        <div className='bg-white p-4 shadow'>
           <div className='grid grid-cols-12 gap-9'>
             <div className='col-span-5'>
               <div className='relative w-full pt-[100%] shadow'>
@@ -31,7 +63,10 @@ const ProductDetail = () => {
                 />
               </div>
               <div className='relative mt-4 grid grid-cols-5 gap-1'>
-                <button className='absolute left-0 top-1/2 z-10 w-5 h-9 -translate-y-1/2 bg-black/20 text-white'>
+                <button
+                  className='absolute left-0 top-1/2 z-10 w-5 h-9 -translate-y-1/2 bg-black/20 text-white'
+                  onClick={prevImage}
+                >
                   <svg
                     xmlns='http://www.w3.org/2000/svg'
                     fill='none'
@@ -47,10 +82,14 @@ const ProductDetail = () => {
                     />
                   </svg>
                 </button>
-                {product.images.slice(0, 5).map((image, index) => {
-                  const isActive = index === 0
+                {currentImages.map((image, index) => {
+                  const isActive = imageActive === image
                   return (
-                    <div className='relative w-full pt-[100%]' key={image}>
+                    <div
+                      className='relative w-full pt-[100%]'
+                      key={image}
+                      onMouseEnter={() => onHoverImage(image)}
+                    >
                       <img
                         className='absolute top-0 left-0 bg-white w-full h-full object-cover'
                         src={image}
@@ -62,7 +101,10 @@ const ProductDetail = () => {
                     </div>
                   )
                 })}
-                <button className='absolute right-0 top-1/2 z-10 w-5 h-9 -translate-y-1/2 bg-black/20 text-white'>
+                <button
+                  className='absolute right-0 top-1/2 z-10 w-5 h-9 -translate-y-1/2 bg-black/20 text-white'
+                  onClick={nextImage}
+                >
                   <svg
                     xmlns='http://www.w3.org/2000/svg'
                     fill='none'
@@ -182,16 +224,18 @@ const ProductDetail = () => {
           </div>
         </div>
       </div>
-      <div className='mt-8 bg-white p-4 shadow'>
-        <div className='rounded bg-gray-50 p-4 text-lg capitalize text-slate-700'>
-          Mô tả sản phẩm
-        </div>
-        <div className='mx-4 mt-12 mb-4 text-sm leading-loose'>
-          <div
-            dangerouslySetInnerHTML={{
-              __html: DOMPurify.sanitize(product.description) //loại bỏ javascript khỏi đây
-            }}
-          ></div>
+      <div className='container'>
+        <div className='mt-8 bg-white p-4 shadow'>
+          <div className='rounded bg-gray-50 p-4 text-lg capitalize text-slate-700'>
+            Mô tả sản phẩm
+          </div>
+          <div className='mx-4 mt-12 mb-4 text-sm leading-loose'>
+            <div
+              dangerouslySetInnerHTML={{
+                __html: DOMPurify.sanitize(product.description) //loại bỏ javascript khỏi đây
+              }}
+            ></div>
+          </div>
         </div>
       </div>
     </div>
