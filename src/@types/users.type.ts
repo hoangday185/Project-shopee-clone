cd1e900070de0f1087ed06484afe338be05cde73
hoangday185 <jarvietnguyen318@gmail.com>
2025-04-06 17:38:52 +0700

    feat: add api for user

--- src/@types/users.type.ts
+++ src/@types/users.type.ts
@@ -5,9 +5,10 @@ export interface User {
   roles: RoleType[];
   email: string;
   name: string;
-  date_of_birth: string;
-  address: string;
-  phone: string;
+  date_of_birth?: string;
+  avatar?: string;
+  address?: string;
+  phone?: string;
   createdAt: string;
   updatedAt: string;
 }
---
+++ src/apis/user.api.ts
@@ -0,0 +1,26 @@
+import { User } from 'src/@types/users.type';
+import { SuccessResponse } from 'src/@types/utils.type';
+import http from 'src/utils/http';
+
+interface BodyUpdateProfile extends Omit<User, 'id' | 'role' | 'createAt' | 'updatedAt'> {
+  password?: string;
+  newPassword?: string;
+}
+
+const userApi = {
+  getProfile() {
+    return http.get<SuccessResponse<User>>('/me');
+  },
+  updateProfile(data: BodyUpdateProfile) {
+    return http.put<SuccessResponse<User>>('/user', data);
+  },
+  uploadAvatar(data: FormData) {
+    return http.post<SuccessResponse<String>>('/user/upload-avatar', data, {
+      headers: {
+        'Content-Type': 'multipart/form-data'
+      }
+    });
+  }
+};
+
+export default userApi;
